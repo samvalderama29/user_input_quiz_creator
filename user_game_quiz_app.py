5a2758d3a64e4c2a4232824bbf8c6e550849141b
Samantha Angel E. Valderama <samanthaangelvalderama@gmail.com>
2025-05-04 20:55:39 +0800

    fix: improved def main_menu() with while loop and with better if-else logic by adding error and goodbye messages

--- user_game_quiz_app.py
+++ user_game_quiz_app.py
@@ -41,24 +41,27 @@ def print_title():
     print(Style.RESET_ALL)
 
 def main_menu():
-    print_title()
-    print(Fore.LIGHTMAGENTA_EX + Style.BRIGHT +  "💡 Welcome to the Quiz Game!")
-    print(Fore.LIGHTWHITE_EX + "📚 Test your general knowledge and see how high you can score!")
+    while True:
+        print_title()
+        print(Fore.LIGHTMAGENTA_EX + Style.BRIGHT +  "💡 Welcome to the Quiz Game!")
+        print(Fore.LIGHTWHITE_EX + "📚 Test your general knowledge and see how high you can score!")
 
-    print(Fore.LIGHTYELLOW_EX + "\nWhat would you like to do?")
-    print("1️⃣ Play")
-    print("2️⃣ Check High Scores")
-    print("3️⃣ Exit")
+        print(Fore.LIGHTYELLOW_EX + "\nWhat would you like to do?")
+        print("1️⃣ Play")
+        print("2️⃣ Check High Scores")
+        print("3️⃣ Exit")
 
-    user_choice = input(Fore.LIGHTYELLOW_EX + "Enter your option (1/2/3): ")
+        user_choice = input(Fore.LIGHTYELLOW_EX + "Enter your option (1/2/3): ")
 
-    if user_choice == "1":
-        quiz_game_start()
-    elif user_choice == "2":
-        high_score_view()
-    elif user_choice == "3":
-        menu_exit()
-        pass
+        if user_choice == "1":
+            quiz_game_start()
+        elif user_choice == "2":
+            high_score_view()
+        elif user_choice == "3":
+            print(Fore.CYAN + Style.BRIGHT + "👋 Goodbye. Thank you for playing!")
+            break
+        else:
+            print(Fore.RED +"\n❌ Invalid input! Please choose between 1, 2, and 3 only.")
 
 def quiz_game_start():
     print()
